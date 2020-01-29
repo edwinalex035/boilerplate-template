1b4c0fc27f09993c79af832c1b5e622f461c5a74
Edwin Sanchez <edwinalex035@gmail.com>
2020-01-29 11:29:37 -0600

    :sparkles: Adding gamification domain
    Adding gamification domain

---
+++ gamification/src/main/java/microservices/book/gamification/repositories/BadgeCardRepository.java
@@ -0,0 +1,20 @@
+package microservices.book.gamification.repositories;
+
+import microservices.book.gamification.models.BadgeCard;
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+/**
+ * Handles data operations with BadgeCards
+ */
+public interface BadgeCardRepository extends
+    CrudRepository<BadgeCard, Long> {
+
+  /**
+   * Retrieves all BadgeCards for a given user.
+   *
+   * @param userId the id of the user to look for BadgeCards
+   * @return the list of BadgeCards, sorted by most recent.
+   */
+  List<BadgeCard> findByUserIdOrderByBadgeTimestampDesc(final Long userId);
+}
