b9f5ee2abf5348e780677999c8d374b371244451
Edwin Sanchez <edwinalex035@gmail.com>
2020-01-30 09:31:11 -0600

    :sparkles: Adding Random Generator Service
    Adding Random Generator Service

---
+++ social-multiplication/src/main/java/microservices/book/multiplication/services/MultiplicationServiceImpl.java
@@ -0,0 +1,27 @@
+package microservices.book.multiplication.services;
+
+import microservices.book.multiplication.models.Multiplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+class MultiplicationServiceImpl implements
+    MultiplicationService {
+
+  private RandomGeneratorService randomGeneratorService;
+
+  @Autowired
+  public MultiplicationServiceImpl(RandomGeneratorService
+      randomGeneratorService) {
+    this.randomGeneratorService = randomGeneratorService;
+  }
+
+  @Override
+  public Multiplication createRandomMultiplication() {
+    int factorA = randomGeneratorService.
+        generateRandomFactor();
+    int factorB = randomGeneratorService.
+        generateRandomFactor();
+    return new Multiplication(factorA, factorB);
+  }
+}
---
+++ social-multiplication/src/main/java/microservices/book/multiplication/services/RandomGeneratorServiceImpl.java
@@ -0,0 +1,13 @@
+package microservices.book.multiplication.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+class RandomGeneratorServiceImpl implements
+    RandomGeneratorService {
+
+  @Override
+  public int generateRandomFactor() {
+    return 0;
+  }
+}
