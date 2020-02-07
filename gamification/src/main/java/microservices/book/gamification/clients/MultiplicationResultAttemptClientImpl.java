cd5e04ec28dcda5563b648f76bd9496423805676
Edwin Sanchez <edwinalex035@gmail.com>
2020-02-07 15:00:28 -0600

    :sparkles: Adding Hytrix for fallback in multiplication attempt
    Adding Hytrix for fallback in multiplication attempt

--- gamification/src/main/java/microservices/book/gamification/clients/MultiplicationResultAttemptClientImpl.java
+++ gamification/src/main/java/microservices/book/gamification/clients/MultiplicationResultAttemptClientImpl.java
@@ -1,5 +1,7 @@
 package microservices.book.gamification.clients;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import lombok.extern.slf4j.Slf4j;
 import microservices.book.gamification.clients.dtos.MultiplicationResultAttempt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
  interface connects to
  * the Multiplication microservice via REST.
  */
+@Slf4j
 @Component
 class MultiplicationResultAttemptClientImpl implements MultiplicationResultAttemptClient {
     private final RestTemplate restTemplate;
@@ -23,9 +26,17 @@ class MultiplicationResultAttemptClientImpl implements MultiplicationResultAttem
         this.multiplicationHost = multiplicationHost;
     }
     @Override
+    @HystrixCommand(fallbackMethod = "defaultResult")
     public MultiplicationResultAttempt retrieveMultiplicationResultAttemptbyId(final Long multiplicationResultAttemptId) {
         return restTemplate.getForObject(
                 multiplicationHost + "/results/" + multiplicationResultAttemptId,
                 MultiplicationResultAttempt.class);
+    }
+
+    private MultiplicationResultAttempt defaultResult(final Long multiplicationResultAttemptId) {
+        log.info("Executing defaultResult method");
+        return new MultiplicationResultAttempt("fakeAlias",
+            10, 10, 100, true);
+
     }
 }
