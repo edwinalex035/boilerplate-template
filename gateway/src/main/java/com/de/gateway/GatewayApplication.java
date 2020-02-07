23b5fe3960617db7d5519c58210db6d49eb3829e
Edwin Sanchez <edwinalex035@gmail.com>
2020-02-07 12:07:11 -0600

    :sparkles: Adding ribbon feature
    Adding ribbon feature

--- gateway/src/main/java/com/de/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/de/gateway/GatewayApplication.java
@@ -4,14 +4,13 @@ import com.de.gateway.conf.RibbonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClientConfiguration;
 import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
-@RibbonClients(defaultConfiguration = RibbonConfig.class)
+//@RibbonClients(defaultConfiguration = RibbonConfig.class)
 public class GatewayApplication {
 
   public static void main(String[] args) {
