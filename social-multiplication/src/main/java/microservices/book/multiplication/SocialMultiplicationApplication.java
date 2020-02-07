26b8dbe2475c662ec8b71efb9faae640026b628e
Edwin Sanchez <edwinalex035@gmail.com>
2020-02-07 10:22:36 -0600

    :sparkles: Adding service discovery feature to microservices
    Adding service discovery feature to microservices

--- social-multiplication/src/main/java/microservices/book/multiplication/SocialMultiplicationApplication.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/SocialMultiplicationApplication.java
@@ -3,6 +3,7 @@ package microservices.book.multiplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SocialMultiplicationApplication {
 
